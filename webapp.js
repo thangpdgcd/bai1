14891f7d45ea116ef334276a63809e4684127c17
dinht <thangpdgcd201784@fpt.edu.vn>
2023-05-19 13:05:52 +0700

    codenodejs

--- webapp.js
+++ webapp.js
@@ -1,13 +1,36 @@
 const http = require('http');
+const hostname = ('localhost');
+const dt = require("./module");
 const port = 3000;
-const hostname = 'localhost';
+
+const fileojb = require('fs');
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plan');
-    res.end('hello world');
+    res.setHeader('Content-Type', 'text/html');
+    switch (req.url) {
+        case "/home":
+            res.writeHead(200);
+            res.end(`<h1>This is the home page</h1>`);
+            break;
+        case "/about":
+            res.writeHead(200);
+            res.end(`This is the about page`);
+            break;
+        case "/compute":
+            res.writeHead(200);
+            res.end(`This is the about  page ${dt.calc(10, 100, "*")}`);
+            break;
+        case "/Listener":
+            res.writeHead('Content-Type', 'text/html')
+
+        default:
+            // res.writeHead();
+            res.end('<h2>Invalid method</h2>');
+            break;
+    }
 });
 
 server.listen(port, hostname, () => {
-    console.log('Server running at  http://${hostname}"${port}/');
+    console.log(`server dang hoat dong o http://${hostname}:${port}`);
 });
